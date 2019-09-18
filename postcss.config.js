1ca97fc5cb3f906f16bc180777bc636a5975b083
Alexey Shatalin <xiaouending@ya.ru>
2019-09-18 16:17:59 +0300

    Rework configurations

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,5 @@
-module.exports = context => ({
+module.exports = ({
   plugins: [
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3,
-      features: {
-        'nesting-rules': true
-      }
-    }),
     require('tailwindcss')('tailwind.config.js'),
     require('autoprefixer')
   ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,80 +1,28 @@
-/*
-
-Tailwind - The Utility-First CSS Framework
-
-A project by Adam Wathan (@adamwathan), Jonathan Reinink (@reinink),
-David Hemphill (@davidhemphill) and Steve Schoger (@steveschoger).
-
-Welcome to the Tailwind config file. This is where you can customize
-Tailwind specifically for your project. Don't be intimidated by the
-length of this file. It's really just a big JavaScript object and
-we've done our very best to explain each section.
-
-View the full documentation at https://tailwindcss.com.
-
-*/
-
 module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '1rem'
+      padding: "1rem"
     },
 
     colors: {
-      transparent: 'transparent',
+      transparent: "transparent",
       primary: {
-        default: '#2f2874',
-        900: '#211e3f'
+        default: "#2f2874",
+        900: "#211e3f"
       },
       secondary: {
-        default: '#f9b000',
-        900: '#df9e00'
+        default: "#f9b000",
+        900: "#df9e00"
       },
       gray: {
-        100: '#fafafa',
-        200: '#edecef',
-        default: '#c3c2ca',
-        400: '#8d8b9b'
+        100: "#fafafa",
+        200: "#edecef",
+        300: "#c3c2ca",
+        400: "#8d8b9b"
       },
-      black: '#2B2B2B',
-      white: '#ffffff'
-    },
-
-    letterSpacing: {
-      tight: '-0.05em',
-      normal: '0',
-      wide: '0.05em',
-      wider: '0.1em',
-      widest: '0.15em'
-    },
-
-    borderWidth: {
-      default: '1px',
-      0: '0',
-      2: '2px',
-      3: '3px',
-      4: '4px',
-      8: '8px'
-    },
-
-    borderColor: theme => ({
-      default: theme('colors.gray.300', 'currentColor'),
-      ...theme('colors')
-    })
-  },
-
-  variants: {
-    borderCollapse: [],
-    outline: ['focus'],
-    fill: [],
-    stroke: [],
-    fontStyle: ['responsive', 'hover', 'focus'],
-    fontSmoothing: ['responsive', 'hover', 'focus'],
-    textTransform: ['responsive', 'hover', 'focus']
-  },
-
-  corePlugins: {
-    container: true
+      black: "#2B2B2B",
+      white: "#ffffff"
+    }
   }
 }
