0e866f11fb975fb46938994282798e37ee83b961
Alexey Shatalin <xiaouending@ya.ru>
2019-09-18 13:26:08 +0300

    Update postcss config & add tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
-module.exports = {
+module.exports = context => ({
   plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-  ],
-}
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3,
+      features: {
+        'nesting-rules': true
+      }
+    }),
+    require('tailwindcss')('tailwind.config.js'),
+    require('autoprefixer')
+  ]
+})
