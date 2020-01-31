2509b0015e1d2390af334ab95871df83662fba93
Alexey Shatalin <xiaouending@ya.ru>
2020-01-31 23:17:52 +0700

    Add cases section

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -86,3 +86,110 @@ SmoothScroll(".smooth-scroll-with-offset", {
   // How far to offset the scrolling anchor location in pixels
   offset: header.offsetHeight
 });
+
+// Cases carousel
+const cases = [
+  {
+    name: 'Сеть АЗС Газпромнефть',
+    logotypesSrc: ['/static/images/cases/Gazpromneft/logo.png'],
+    previewSrc: '/static/images/cases/Gazpromneft/preview.jpg',
+    durationYears: '2017-2018',
+    durationMonths: 'Ноябрь – Февраль',
+    durationTime: '13 + 1 неделя'
+  },
+  {
+    name: 'Пятёрочка',
+    logotypesSrc: ['/static/images/cases/Pyaterochka/logo.png'],
+    previewSrc: '/static/images/cases/Pyaterochka/preview.jpg',
+    durationYears: '2016',
+    durationMonths: 'Июль - Октябрь',
+    durationTime: '13 + 1 неделя'
+  },
+  {
+    name: 'Виктория',
+    logotypesSrc: ['/static/images/cases/Lukoyl/logo.png'],
+    previewSrc: '/static/images/cases/Lukoyl/preview.jpg',
+    durationYears: '2019',
+    durationMonths: 'Апрель - Июнь',
+    durationTime: '10 + 6 недель'
+  },
+  {
+    name: 'Карусель',
+    logotypesSrc: ['/static/images/cases/Karusel/logo.png'],
+    previewSrc: '/static/images/cases/Karusel/preview.jpg',
+    durationYears: '2018',
+    durationMonths: 'Февраль - Апрель',
+    durationTime: '7 + 6 недель'
+  },
+  {
+    name: 'Мираторг',
+    logotypesSrc: ['/static/images/cases/Miratorg/logo1.png','/static/images/cases/Miratorg/logo2.gif'],
+    previewSrc: '/static/images/cases/Miratorg/preview.jpg',
+    durationYears: '2016',
+    durationMonths: 'Апрель - Июль',
+    durationTime: '11 + 6 недель'
+  },
+  {
+    name: 'ВР',
+    logotypesSrc: ['/static/images/cases/BP/logo.png'],
+    previewSrc: '/static/images/cases/BP/preview.jpg',
+    durationYears: '2019-2020',
+    durationMonths: 'Апрель - Июль',
+    durationTime: '11 + 4 недели'
+  },
+  {
+    name: 'СПАР',
+    logotypesSrc: ['/static/images/cases/SPAR/logo.png'],
+    previewSrc: '/static/images/cases/SPAR/preview.jpg',
+    durationYears: '2018-2019',
+    durationMonths: 'Ноябрь - Февраль',
+    durationTime: '13 + 6 недель'
+  },
+  {
+    name: 'Сеть АЗС Газпромнефть',
+    logotypesSrc: ['/static/images/cases/Gazpromneft_2/logo1.png','/static/images/cases/Gazpromneft_2/logo2.png'],
+    previewSrc: '/static/images/cases/Gazpromneft_2/preview.jpg',
+    durationYears: '2019',
+    durationMonths: 'Апрель - Июль',
+    durationTime: '14 + 5 недель'
+  },
+  {
+    name: 'SMALL',
+    logotypesSrc: ['/static/images/cases/SMALL/logo.png'],
+    previewSrc: '/static/images/cases/SMALL/preview.jpg',
+    durationYears: '2019-2020',
+    durationMonths: 'Октябрь - Январь',
+    durationTime: '13 + 1 неделя'
+  },
+  {
+    name: 'Беларуснефть',
+    logotypesSrc: ['/static/images/cases/Belorusneft/logo.png'],
+    previewSrc: '/static/images/cases/Belorusneft/preview.jpg',
+    durationYears: '2019',
+    durationMonths: 'Февраль - Май',
+    durationTime: '13 + 1 неделя'
+  },
+]
+
+const casesCarousel = document.querySelector('.cases-carousel')
+
+cases.map(element => {
+  const caseItem = casesCarousel.querySelector('.case').cloneNode(true)
+
+  caseItem.querySelector('.name').textContent = element.name
+  element.logotypesSrc.map(logotypeSrc => {
+    const logo = caseItem.querySelector('.image-logo').cloneNode(true)
+    logo.src = logotypeSrc
+    logo.classList.remove('hidden')
+    caseItem.querySelector('.image-logo').after(logo)
+  })
+  caseItem.querySelector('.image-logo').alt = element.name
+  caseItem.querySelector('.image-preview').src = element.previewSrc
+  caseItem.querySelector('.image-preview').alt = element.name
+  caseItem.querySelector('.duration-years').textContent = element.durationYears
+  caseItem.querySelector('.duration-months').textContent = element.durationMonths
+  caseItem.querySelector('.duration-time').textContent = element.durationTime
+
+  caseItem.classList.remove('hidden')
+  casesCarousel.appendChild(caseItem)
+})
